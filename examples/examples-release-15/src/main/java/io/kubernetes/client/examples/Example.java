4e6a07888e0b64fc2887774ba828aa06cc7fffd1
Shreyoshi Mahato <shreyoshimahato@gmail.com>
2022-12-09 02:25:01 +0530

    added some comments as Kubernetes concepts are complex

--- examples/examples-release-15/src/main/java/io/kubernetes/client/examples/Example.java
+++ examples/examples-release-15/src/main/java/io/kubernetes/client/examples/Example.java
@@ -27,8 +27,10 @@ public class Example {
         Configuration.setDefaultApiClient(apiClient);
 
         CoreV1Api coreV1Api = new CoreV1Api();
+        // PodList is a list of Pods.
         V1PodList pods =
                 coreV1Api.listPodForAllNamespaces(null, null, null, null, null, null, null, null, null, null);
+        // pods.getItems() are List<V1Pod>
         if (pods != null && CollectionUtils.isNotEmpty(pods.getItems())) {
             pods.getItems().forEach((pod) -> System.out.println(pod.getMetadata().getName()));
         }
