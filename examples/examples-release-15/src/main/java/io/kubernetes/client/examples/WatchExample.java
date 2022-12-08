4fba92e515aeca89f5b16431344c7a1daa425d20
Shreyoshi Mahato <shreyoshimahato@gmail.com>
2022-12-09 02:10:28 +0530

    separated direct usage of 5, true to Constant, separated several nested methods with a variable for readibility

--- examples/examples-release-15/src/main/java/io/kubernetes/client/examples/WatchExample.java
+++ examples/examples-release-15/src/main/java/io/kubernetes/client/examples/WatchExample.java
@@ -13,29 +13,48 @@ import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 import okhttp3.OkHttpClient;
+import okhttp3.Call;
+
+import java.lang.reflect.Type;
 
 /**
  * A simple example of how to use Watch API to watch changes in Namespace list.
+ * <p>https://github.com/kubernetes-client/java/wiki/3.-Code-Examples
+ * <p>watch on namespace object
  */
 public class WatchExample {
+    private static final Integer LIMIT = 5;
+    private static final Boolean WATCH = Boolean.TRUE;
+
     public static void main(String[] args) throws IOException, ApiException {
-        ApiClient client = Config.defaultClient();
+        ApiClient apiClient = Config.defaultClient();
         // infinite timeout
         OkHttpClient httpClient =
-                client.getHttpClient().newBuilder().readTimeout(0, TimeUnit.SECONDS).build();
-        client.setHttpClient(httpClient);
-        Configuration.setDefaultApiClient(client);
+                apiClient.getHttpClient().newBuilder().readTimeout(0, TimeUnit.SECONDS).build();
+        apiClient.setHttpClient(httpClient);
+        Configuration.setDefaultApiClient(apiClient);
 
-        CoreV1Api api = new CoreV1Api();
+        CoreV1Api coreV1Api = new CoreV1Api();
 
-        Watch<V1Namespace> watch =
-                Watch.createWatch(
-                        client,
-                        api.listNamespaceCall(
-                                null, null, null, null, null, 5, null, null, null, Boolean.TRUE, null),
-                        new TypeToken<Watch.Response<V1Namespace>>() {
-                        }.getType());
+        /**
+         * watch flag is set in the call
+         * CoreV1Api.listNamespaceCall and set watch to True and watch the changes to namespaces.
+         */
+        Call call = coreV1Api.listNamespaceCall(
+                null, null, null, null, null, LIMIT, null, null, null, WATCH, null);
 
+        /**
+         * watchType The type of the WatchResponse
+         */
+        Type watchType = new TypeToken<Watch.Response<V1Namespace>>() {
+        }.getType();
+
+        /**
+         * Creates a watch on a V1Namespace using an API Client and a Call object.
+         * @return Watch object on V1Namespace
+         * @throws ApiException on IO exceptions.
+         */
+        Watch<V1Namespace> watch = Watch.createWatch(apiClient, call, watchType);
         try {
             for (Watch.Response<V1Namespace> item : watch) {
                 System.out.printf("%s : %s%n", item.type, item.object.getMetadata().getName());
