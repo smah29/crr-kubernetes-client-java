b5f6201e0392fa1af3aa11413a736048d4700ed1
Shreyoshi Mahato <shreyoshimahato@gmail.com>
2022-12-09 02:45:22 +0530

    reduced the intent from 4 to 2 in preferences editor codestyle java, to match the original author format

--- examples/examples-release-15/src/main/java/io/kubernetes/client/examples/Example.java
+++ examples/examples-release-15/src/main/java/io/kubernetes/client/examples/Example.java
@@ -22,17 +22,17 @@ import java.io.IOException;
  * <p>list all pods
  */
 public class Example {
-    public static void main(String[] args) throws IOException, ApiException {
-        ApiClient apiClient = Config.defaultClient();
-        Configuration.setDefaultApiClient(apiClient);
+  public static void main(String[] args) throws IOException, ApiException {
+    ApiClient apiClient = Config.defaultClient();
+    Configuration.setDefaultApiClient(apiClient);
 
-        CoreV1Api coreV1Api = new CoreV1Api();
-        // PodList is a list of Pods.
-        V1PodList pods =
-                coreV1Api.listPodForAllNamespaces(null, null, null, null, null, null, null, null, null, null);
-        // pods.getItems() are List<V1Pod>
-        if (pods != null && CollectionUtils.isNotEmpty(pods.getItems())) {
-            pods.getItems().forEach((pod) -> System.out.println(pod.getMetadata().getName()));
-        }
+    CoreV1Api coreV1Api = new CoreV1Api();
+    // PodList is a list of Pods.
+    V1PodList pods =
+            coreV1Api.listPodForAllNamespaces(null, null, null, null, null, null, null, null, null, null);
+    // pods.getItems() are List<V1Pod>
+    if (pods != null && CollectionUtils.isNotEmpty(pods.getItems())) {
+      pods.getItems().forEach((pod) -> System.out.println(pod.getMetadata().getName()));
     }
+  }
 }
--- examples/examples-release-15/src/main/java/io/kubernetes/client/examples/WatchExample.java
+++ examples/examples-release-15/src/main/java/io/kubernetes/client/examples/WatchExample.java
@@ -23,44 +23,44 @@ import java.lang.reflect.Type;
  * <p>watch on namespace object
  */
 public class WatchExample {
-    private static final Integer LIMIT = 5;
-    private static final Boolean WATCH = Boolean.TRUE;
+  private static final Integer LIMIT = 5;
+  private static final Boolean WATCH = Boolean.TRUE;
 
-    public static void main(String[] args) throws IOException, ApiException {
-        ApiClient apiClient = Config.defaultClient();
-        // infinite timeout
-        OkHttpClient httpClient =
-                apiClient.getHttpClient().newBuilder().readTimeout(0, TimeUnit.SECONDS).build();
-        apiClient.setHttpClient(httpClient);
-        Configuration.setDefaultApiClient(apiClient);
+  public static void main(String[] args) throws IOException, ApiException {
+    ApiClient apiClient = Config.defaultClient();
+    // infinite timeout
+    OkHttpClient httpClient =
+            apiClient.getHttpClient().newBuilder().readTimeout(0, TimeUnit.SECONDS).build();
+    apiClient.setHttpClient(httpClient);
+    Configuration.setDefaultApiClient(apiClient);
 
-        CoreV1Api coreV1Api = new CoreV1Api();
+    CoreV1Api coreV1Api = new CoreV1Api();
 
-        /**
-         * watch flag is set in the call
-         * CoreV1Api.listNamespaceCall and set watch to True and watch the changes to namespaces.
-         */
-        Call call = coreV1Api.listNamespaceCall(
-                null, null, null, null, null, LIMIT, null, null, null, WATCH, null);
+    /**
+     * watch flag is set in the call
+     * CoreV1Api.listNamespaceCall and set watch to True and watch the changes to namespaces.
+     */
+    Call call = coreV1Api.listNamespaceCall(
+            null, null, null, null, null, LIMIT, null, null, null, WATCH, null);
 
-        /**
-         * watchType is the type of the WatchResponse after calling createWatch method
-         */
-        Type watchType = new TypeToken<Watch.Response<V1Namespace>>() {
-        }.getType();
+    /**
+     * watchType is the type of the WatchResponse after calling createWatch method
+     */
+    Type watchType = new TypeToken<Watch.Response<V1Namespace>>() {
+    }.getType();
 
-        /**
-         * Creates a watch on a V1Namespace using an API Client and a Call object.
-         * @return Watch object on V1Namespace
-         * @throws ApiException on IO exceptions.
-         */
-        Watch<V1Namespace> watch = Watch.createWatch(apiClient, call, watchType);
-        try {
-            for (Watch.Response<V1Namespace> item : watch) {
-                System.out.printf("%s : %s%n", item.type, item.object.getMetadata().getName());
-            }
-        } finally {
-            watch.close();
-        }
+    /**
+     * Creates a watch on a V1Namespace using an API Client and a Call object.
+     * @return Watch object on V1Namespace
+     * @throws ApiException on IO exceptions.
+     */
+    Watch<V1Namespace> watch = Watch.createWatch(apiClient, call, watchType);
+    try {
+      for (Watch.Response<V1Namespace> item : watch) {
+        System.out.printf("%s : %s%n", item.type, item.object.getMetadata().getName());
+      }
+    } finally {
+      watch.close();
     }
+  }
 }
