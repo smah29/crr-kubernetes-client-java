a8e694f9e5824729c59cd5e299490c3f878d0ca4
Shreyoshi Mahato <shreyoshimahato@gmail.com>
2022-12-09 02:57:18 +0530

    YamlExample : added comment to explain what this class is for, removed license

--- examples/examples-release-15/src/main/java/io/kubernetes/client/examples/YamlExample.java
+++ examples/examples-release-15/src/main/java/io/kubernetes/client/examples/YamlExample.java
@@ -1,15 +1,3 @@
-/*
-Copyright 2020 The Kubernetes Authors.
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-http://www.apache.org/licenses/LICENSE-2.0
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
 package io.kubernetes.client.examples;
 
 import io.kubernetes.client.custom.IntOrString;
@@ -36,6 +24,7 @@ import java.util.HashMap;
  * -Dexec.mainClass="io.kubernetes.client.examples.YamlExample"
  *
  * <p>From inside $REPO_DIR/examples
+ * <p>Suggested way to load or dump resource in Yaml.
  */
 public class YamlExample {
   public static void main(String[] args) throws IOException, ApiException, ClassNotFoundException {
