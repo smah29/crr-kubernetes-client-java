5e6a580a90046fd813278f8e2ad1714b2d36c504
Shreyoshi Mahato <shreyoshimahato@gmail.com>
2022-12-10 04:38:38 +0530

    const declaration, remove repetition, code small by refactoring by extract to method

--- examples/examples-release-15/src/main/java/io/kubernetes/client/examples/GenericClientExample.java
+++ examples/examples-release-15/src/main/java/io/kubernetes/client/examples/GenericClientExample.java
@@ -1,19 +1,8 @@
-/*
-Copyright 2020 The Kubernetes Authors.
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-http://www.apache.org/licenses/LICENSE-2.0
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
 package io.kubernetes.client.examples;
 
 import io.kubernetes.client.custom.V1Patch;
 import io.kubernetes.client.openapi.ApiClient;
+import io.kubernetes.client.openapi.ApiException;
 import io.kubernetes.client.openapi.models.V1Container;
 import io.kubernetes.client.openapi.models.V1ObjectMeta;
 import io.kubernetes.client.openapi.models.V1Pod;
@@ -21,43 +10,73 @@ import io.kubernetes.client.openapi.models.V1PodList;
 import io.kubernetes.client.openapi.models.V1PodSpec;
 import io.kubernetes.client.util.ClientBuilder;
 import io.kubernetes.client.util.generic.GenericKubernetesApi;
+
 import java.util.Arrays;
+import java.util.List;
+
+import lombok.Getter;
+import lombok.AllArgsConstructor;
 
 public class GenericClientExample {
+  private static final String NAMESPACE = "default";
+  private static final String POD_NAME = "foo";
+  private static final String API_VERSION = "v1";
+  private static final String RESOURCE = "pods";
 
+  // The following codes demonstrates using generic client to manipulate pods
   public static void main(String[] args) throws Exception {
-
-    // The following codes demonstrates using generic client to manipulate pods
-    V1Pod pod =
-        new V1Pod()
-            .metadata(new V1ObjectMeta().name("foo").namespace("default"))
-            .spec(
-                new V1PodSpec()
-                    .containers(Arrays.asList(new V1Container().name("c").image("test"))));
 
     ApiClient apiClient = ClientBuilder.standard().build();
     GenericKubernetesApi<V1Pod, V1PodList> podClient =
-        new GenericKubernetesApi<>(V1Pod.class, V1PodList.class, "", "v1", "pods", apiClient);
+        new GenericKubernetesApi<>(V1Pod.class, V1PodList.class, "", API_VERSION, RESOURCE, apiClient);
 
-    V1Pod latestPod = podClient.create(pod).throwsApiException().getObject();
+    MyContainer container = new MyContainer("c", "test");
+    V1Pod pod = getPod(NAMESPACE, POD_NAME, container);
+
+    createPod(pod, podClient);
+
+    patchPod(podClient);
+
+    deletePod(podClient);
+  }
+
+  private static void createPod(V1Pod pod, GenericKubernetesApi<V1Pod, V1PodList> podClient) throws ApiException {
+    podClient.create(pod).throwsApiException().getObject();
     System.out.println("Created!");
+  }
 
-    V1Pod patchedPod =
-        podClient
-            .patch(
-                "default",
-                "foo",
-                V1Patch.PATCH_FORMAT_STRATEGIC_MERGE_PATCH,
-                new V1Patch("{\"metadata\":{\"finalizers\":[\"example.io/foo\"]}}"))
-            .throwsApiException()
-            .getObject();
+  private static void patchPod(GenericKubernetesApi<V1Pod, V1PodList> podClient) throws ApiException {
+    podClient
+        .patch(
+            NAMESPACE,
+            POD_NAME,
+            V1Patch.PATCH_FORMAT_STRATEGIC_MERGE_PATCH,
+            new V1Patch("{\"metadata\":{\"finalizers\":[\"example.io/foo\"]}}"))
+        .throwsApiException()
+        .getObject();
     System.out.println("Patched!");
+  }
 
-    V1Pod deletedPod = podClient.delete("default", "foo").throwsApiException().getObject();
+  private static void deletePod(GenericKubernetesApi<V1Pod, V1PodList> podClient) throws ApiException {
+    V1Pod deletedPod = podClient.delete(NAMESPACE, POD_NAME).throwsApiException().getObject();
     if (deletedPod != null) {
       System.out.println(
           "Received after-deletion status of the requested object, will be deleting in background!");
     }
     System.out.println("Deleted!");
   }
+
+  private static V1Pod getPod(String namespace, String podName, MyContainer container) {
+    List<V1Container> containers = Arrays.asList(new V1Container().name(container.getName()).image(container.getImage()));
+    V1ObjectMeta metadata = new V1ObjectMeta().name(podName).namespace(namespace);
+    return new V1Pod().metadata(metadata).spec(new V1PodSpec().containers(containers));
+  }
+
+  @AllArgsConstructor
+  @Getter
+  static class MyContainer {
+    String name;
+    String image;
+  }
+
 }
